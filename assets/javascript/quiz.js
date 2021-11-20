43aa0dd2065a653fd3948e8770705d9616817c4f
Austin Wonka <wonkaaustin@gmail.com>
2021-11-20 15:34:32 -0500

    everything is done, all is working!

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,16 +1,16 @@
 const question = document.querySelector('#question')
-const choices = document.querySelector('.choice-text')
+const choices = document.querySelectorAll('.choice-text')
 const questionText = document.querySelector('#questionText')
-const timerText = document.querySelector('#timerText')
 const scoreText = document.querySelector('#score')
 const progressTimer = document.querySelector('#timer')
 
+let sec = 60
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
-let timerCounter = '60'
+let timerCounter = 0
 
 let questions = [
     {
@@ -63,14 +63,28 @@ startGame = () => {
     score = 0
     availableQuestions = [...questions]
     getNewQuestion()
+    timerStart()
     // May need to Set timer here too
 }
+
+function timerStart() {
+    sec--;
+    var timer = setInterval(function(){
+        document.getElementById('timerText').innerHTML='00:'+sec;
+        sec--;
+        if (sec <= 0) {
+            clearInterval(timer)
+            return window.location.assign('./index.html')
+        }
+    }, 1000)
+}
+
 
 getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('./end.html')
     }
 
     questionCounter++
@@ -81,6 +95,46 @@ getNewQuestion = () => {
     question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
-        
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion ['choice' + number]
     })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
 }
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+        acceptingAnswers = false
+
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        } else{classToApply === 'incorrect'
+        sec += -15
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+    })
+})
+
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score
+}
+
+
+
+startGame()
